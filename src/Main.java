211c5495be11d5376560b72a6e13bd8926031954
SerhiyKoval <kovalss2006@gmail.com>
2026-02-02 22:51:15 +0200

    Add generateMatrix

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+
 class Main {
+    public static int[][] generateMatrix(int n) {
+        int[][] matrix = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                matrix[i][j] = 1;
+            }
+        }
+        return matrix;
+    }
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        int[] N = {100,500,1000,2000,5000,10000,20000,30000};
+        for (int n : N) {
+
+            long startTime = System.nanoTime();
+            int[][] A = generateMatrix(n);
+            long endTime = System.nanoTime();
+            double durationMs = (endTime - startTime) / 1000000.0;
+            System.out.println("Time taken to generate " + n + "x" + n + " matrix: " + durationMs + " ms");
+        }
+
     }
 }
